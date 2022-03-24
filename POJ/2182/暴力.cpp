b60c54b482fd15610c31d30223035358c5ce5581
TPam <87828006+FTPam@users.noreply.github.com>
2022-03-24 21:57:13 +0800

    Update 暴力.cpp
    改为符合POJ要求的输入输出

--- POJ/2182/暴力.cpp
+++ POJ/2182/暴力.cpp
@@ -9,7 +9,9 @@ int main() {
 	int c[MAX];	// 输出
 	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++) {
+	a[0] = 0;
+	b[0] = 1;
+	for (int i = 1; i < n; i++) {
 		cin >> a[i];
 		b[i] = i + 1;
 	}
@@ -27,6 +29,6 @@ int main() {
 		}
 	}
 	for (int i = 0; i < n; i++) {
-		cout << c[i] << " ";
+		cout << c[i] << endl;
 	}
 }
