7f9f1332764b5f0d16f7da52cb1aa4e295566af7
TPam <87828006+FTPam@users.noreply.github.com>
2022-03-25 23:49:21 +0800

    优化维护算法（借鉴书上的）

--- POJ/2182/线段树.cpp
+++ POJ/2182/线段树.cpp
@@ -7,7 +7,6 @@ typedef struct Tree {
 }tree;
 
 tree t[40000];
-int pre;
 
 void buildTree(int start, int end, int n) {
 	t[n] = { start, end, end - start + 1 };
@@ -18,29 +17,15 @@ void buildTree(int start, int end, int n) {
 	buildTree((start + end) / 2 + 1, end, 2 * n + 1);
 }
 
-int query(int n) {
-	if (t[n].total == 0) {
-		return 0;
-	}
-	if (t[n].start == t[n].end) {	// 如果找到叶子
-		if (t[n].total == 1 && pre == 0) {		// 说明是要找的那个叶子
-			t[n].total = 0;
-			return t[n].start;
-		}
-		else {	// 是前面的叶子
-			pre--;
-			return 0;
-		}
-	}
-	int a = query(2 * n);
-	if (a == 0) {
-		a = query(2 * n + 1);
-		if (a == 0) {
-			return 0;
-		}
-	}
+int query(int pre, int n) {
 	t[n].total--;
-	return a;
+	if (t[n].start == t[n].end) {
+		return t[n].start;
+	}
+	if (pre > t[2 * n].total) {
+		return query(pre - t[2 * n].total, 2 * n + 1);
+	}
+	return query(pre, 2 * n);
 }
 
 int main() {
@@ -54,8 +39,7 @@ int main() {
 	}
 	buildTree(1, n, 1);
 	for (int i = n - 1; i >= 0; i--) {
-		pre = a[i];
-		c[i] = query(1);
+		c[i] = query(a[i] + 1, 1);
 	}
 	for (int i = 0; i < n; i++) {
 		cout << c[i] << endl;
